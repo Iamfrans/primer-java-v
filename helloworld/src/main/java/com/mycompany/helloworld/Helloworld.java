40f5720aa1a213e5debeced425ce5a1e92fa879e
Iamframs <132354@senati.pe>
2023-03-09 11:39:05 -0500

    I

---
+++ helloworld/src/main/java/com/mycompany/helloworld/Helloworld.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("SENATINOS");
+    }
+}
