f2f4b1e509837dcfd7dbb9863fb490a48f82be28
Iamframs <132354@senati.pe>
2023-03-10 07:32:34 -0500

    Borrando lo innecesario

--- helloworld/src/main/java/com/mycompany/helloworld/Helloworld.java
+++ helloworld/src/main/java/com/mycompany/helloworld/Helloworld.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-package com.mycompany.helloworld;
-
-/**
- *
- * @author Alumno
- */
 public class Helloworld {
 
     public static void main(String[] args) {
